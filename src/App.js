a064e71371032afc9d98c932e5fcfdee87f26310
Liana <liana.stathara@propep.de>
2021-11-29 11:16:04 +0100

    added particle animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Navigation from './screens/navigationBar/NavigationBar'
+import ParticleAnimation from 'react-particle-animation'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <ParticleAnimation 
+      style={{ height: '96vh' }}
+      background={{r: 21, g: 22, b: 23, a: 1 }}
+      particleSpeed={0.1}
+      particleRadius={0.8}
+      color={{r: 158, g: 217, b: 249, a: 255 }}
+      />
     </div>
   );
 }
